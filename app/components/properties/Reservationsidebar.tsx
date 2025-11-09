3daaa9409bc891a28947d12d738fff987219f3d7
Glene Miko Jaudian <glenemiko.jaudian@msunaawan.edu.ph>
2025-11-09 12:57:48 +0800

    Landlord page (static)- GLENE MIKO A. JAUDIAN

--- app/components/properties/Reservationsidebar.tsx
+++ app/components/properties/Reservationsidebar.tsx
@@ -1,4 +1,4 @@
-const ReservationSidebar = () =>{
+const ReservationSidebar = () => {
     return (
         <aside className="mt-6 p-6 col-span-2 rounded-xl border border-gray-300 shadow-xl">
             <h2 className="mb-5 text-2xl">$200 per night</h2>
