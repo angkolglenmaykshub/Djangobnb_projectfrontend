4c05936b94973632c0e9602039dcfa8fbaf5ddc5
Glene Miko Jaudian <glenemiko.jaudian@msunaawan.edu.ph>
2025-11-12 21:59:04 +0800

    Video-2 Sign up modal (remember error codes)- GLENE MIKO A. JAUDIAN

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
 import Modal from "./components/modals/Modal";
 import LoginModal from "./components/modals/LoginModal";
+import SignUpModal from "./components/modals/SignupModal";
 
 
 const geistSans = Geist({
@@ -46,6 +47,7 @@ export default function RootLayout({
           {children}
         </div>
         <LoginModal />
+        <SignUpModal />
       </body>
     </html>
   );
