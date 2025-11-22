86b5d6e7dbfc65a27a936249033350dd6d69d39c
Glene Miko <glenemiko.jaudian@msunaawan.edu.ph>
2025-11-22 14:00:20 +0800

    Video 3 Last part- Get properties in the frontend and list them -Glene Miko Jaudian

--- app/components/properties/PropertyListItem.tsx
+++ app/components/properties/PropertyListItem.tsx
@@ -1,28 +1,41 @@
-import Image from "next/image"
+import { PropertyType } from "./PropertyList";
 
-const PropertyListItem = () =>{
-    return (
-       <div className="cursor-pointer">
-            <div className="relative overflow-hidden aspect-square rounded-xl">
-                <Image
-                    fill
-                    src='/beach_1.jpg'
-                    sizes="(max-width: 768px) 768px, (max-width: 1200px): 768px, 768px"
-                    className="hover:scale-110 object-cover transition h-full w-full"
-                    alt="Beach House"
-                />
 
-            </div>
 
-            <div className="mt-2">
-                <p className="tsxt-lg font-bold">Property Name</p>
 
-            </div>
-            <div className="mt-2">
-                <p className="text-sm text-gray-500"><strong>$200</strong> per night</p>
-            </div>
-       </div>
-    )
+interface PropertyProps {
+    property: PropertyType,
+    markFavorite?: (is_favorite: boolean) => void;
 }
 
+
+const PropertyListItem: React.FC<PropertyProps> = ({
+    property,
+}) => {
+    return (
+        <div className="cursor-pointer">
+            <div className="relative overflow-hidden aspect-square rounded-xl">
+                <img
+                    src={property.image_url}
+                    className="hover:scale-110 object-cover transition h-full w-full"
+                    alt="Beach house"
+                />
+            </div>
+
+
+            <div className="mt-2">
+                <p className="text-lg font-bold">{property.title}</p>
+            </div>
+
+
+            <div className="mt-2">
+                <p className="text-sm text-gray-700">
+                    <strong>{property.price_per_night}</strong> per night
+                </p>
+            </div>
+
+
+        </div>
+    )
+}
 export default PropertyListItem;
