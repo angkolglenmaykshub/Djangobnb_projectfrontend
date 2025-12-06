b7be6aaf93eca0ce2d579df86921c8c16ec8a436
Glene Miko <glenemiko.jaudian@msunaawan.edu.ph>
2025-12-06 17:27:43 +0800

    Video 6 -Make it possible to set property as favorite- Glene Miko Jaudian

--- app/components/properties/PropertyListItem.tsx
+++ app/components/properties/PropertyListItem.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { PropertyType } from "./PropertyList";
 import { useRouter } from "next/navigation";
+import FavoriteButton from "../FavoriteButton";
 
 
 interface PropertyProps {
@@ -10,28 +11,34 @@ interface PropertyProps {
 
 
 const PropertyListItem: React.FC<PropertyProps> = ({
-    property
+    property,
+    markFavorite
 }) => {
     const router = useRouter();
-    const apiHost = process.env.NEXT_PUBLIC_API_HOST || 'http://localhost:8000';
 
 
     return (
         <div
             className="cursor-pointer"
             onClick={() => router.push(`/properties/${property.id}`)}
-
-
         >
             <div className="relative overflow-hidden aspect-square rounded-xl">
                 <Image
-                    unoptimized
-                    src={property.image_url}
-                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
                     fill
+                    src={property.image_url}
+                    sizes="(max-width: 768px) 768px, (max-width: 1200px): 768px, 768px"
                     className="hover:scale-110 object-cover transition h-full w-full"
                     alt="Beach house"
                 />
+
+
+                {markFavorite && (
+                    <FavoriteButton
+                        id={property.id}
+                        is_favorite={property.is_favorite}
+                        markFavorite={(is_favorite) => markFavorite(is_favorite)}
+                    />
+                )}
             </div>
 
 
@@ -41,13 +48,11 @@ const PropertyListItem: React.FC<PropertyProps> = ({
 
 
             <div className="mt-2">
-                <p className="text-sm text-gray-700">
-                    <strong>{property.price_per_night}</strong> per night
-                </p>
+                <p className="text-sm text-gray-500"><strong>${property.price_per_night}</strong> per night</p>
             </div>
-
-
         </div>
     )
 }
+
+
 export default PropertyListItem;
